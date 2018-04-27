ed4f1471654682ca313a12e7b04e60b351a14194
MCSmallW <496432813@qq.com>
2018-04-27 10:54:30 +0800

    ps. 修改bug工号和反馈信息同时为空问题

--- Check/AddSQL.cs
+++ Check/AddSQL.cs
@@ -16,21 +16,25 @@ namespace Check
                 string sql = "";
                 SQLHelper helper = new SQLHelper();
                 helper.OperateSql(sql);
+                return;
             }
             else if(JobNum==null)
             {
                 //页面显示弹窗 请输入工号
 
+                return;
             }
             else if(Bug==null)
             {
                 //弹窗显示 你的反馈内容为空
 
+                return;
             }
             else
             {
                 //弹窗显示错误
 
+                return;
             }
         }
     }
